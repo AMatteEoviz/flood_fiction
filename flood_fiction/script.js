b1989a5051fa3f41f943f91d22667d043fc76cc4
AMatteEoviz <ymatte@mailo.com>
2025-04-11 18:02:01 +0200

    Update script.js

--- flood_fiction/script.js
+++ flood_fiction/script.js
@@ -1,44 +1,38 @@
-document.getElementById("selection").addEventListeners("change", (event) => {
-                                            const value = document.getElementById('inputValue').value;
-                                            const url = `https://github.com/AMatteEoviz/flood_fiction/blob/main/GEOJSON/hexa100_altiMean_popMean_${value}.geojson`;
-  
-  var spec = {
-  
+document.getElementById("select").addEventListener("change", (event) => {
+  const value = document.getElementById('select').value.toLowerCase();
+
+  const url = `https://raw.githubusercontent.com/AMatteEoviz/flood_fiction/main/GEOJSON/hexa100_altiMean_popMean_${value}.geojson`;
+
+  const spec = {
     "$schema": "https://vega.github.io/schema/vega-lite/v5.21.0.json",
     "width": 600,
     "height": 500,
     "autosize": "fit",
-  
-  
+
     "data": {
       "url": url,
-      "format": {"type": "json", "property": "features"}
+      "format": { "type": "json", "property": "features" }
     },
-  
+
     "layer": [
       {
-        "mark":{"type": "geoshape"},
-        encoding: {
-            "fill": {"value": "black"},
-            "stroke": {"value": "black"},
-            "strokeWidth": {"value": 7},
-            "opacity": {"value": 0.2}
+        "mark": { "type": "geoshape" },
+        "encoding": {
+          "fill": { "value": "black" },
+          "stroke": { "value": "black" },
+          "strokeWidth": { "value": 7 },
+          "opacity": { "value": 0.2 }
         }
-        },
-  
+      },
       {
-      "mark":{"type": "geoshape"},
-      "encoding": {
-          "fill": {"value": "#252563"},
-          "stroke": {"value": "white"}
-      }
+        "mark": { "type": "geoshape" },
+        "encoding": {
+          "fill": { "value": "#252563" },
+          "stroke": { "value": "white" }
+        }
       }
     ]
-  
-  }
-  
-  
+  };
+
   vegaEmbed('#maCarte', spec).catch(console.error);
-}
-
-
+});
